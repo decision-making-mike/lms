decision-making-mike/lms

commit bb23a695a21274c1fb97b9d2d86e92b2702eac0f

Task list HTML version creation unification
- There has been unified the creation of the HTML versions of task lists. I'm increasing the minor version since there changed the way the "(NA)" task is shown in the all task list.

index.php
@@ -5,8 +5,11 @@
 
     function get_task_html (
         $base_url,
+        $task_list,
         $tasks,
-        $task
+        $task,
+        ?callable
+            $get_decorated_task_list_item_html
     ) {
         $task_html
             = htmlspecialchars_with_ent_quotes(
@@ -29,7 +32,7 @@
             = $task === '(NA)'
                 ? '(NA)'
                 : htmlspecialchars_with_ent_quotes(
-                    $tasks[$task][1]
+                    $task_list[$task][1]
                 );
 
         # Get the information whether any
@@ -73,12 +76,46 @@
                 ];
         }
 
+        if (
+            $get_decorated_task_list_item_html
+                !== null
+        ) {
+            $task_html
+                = $get_decorated_task_list_item_html(
+                    $task_html
+                );
+        }
+
         return
             "({$status_html}) "
             . $child_task_pending_information_html
             . "<a href=\"{$url_html}\">"
             . $task_html
             . '</a>';
+    }
+
+    function get_task_list_html (
+        $base_url,
+        $task_list,
+        $all_task_list,
+        callable
+            $get_decorated_task_list_item_html
+                = null
+    ) {
+        $task_list_content_html = '';
+        foreach ($task_list as $task_name => $_) {
+            $task_list_item_content_html
+                = get_task_html(
+                    $base_url,
+                    $task_list,
+                    $all_task_list,
+                    $task_name,
+                    $get_decorated_task_list_item_html
+                );
+            $task_list_content_html
+                .= "<li>{$task_list_item_content_html}</li>";
+        }
+        return "<ul>{$task_list_content_html}</ul>";
     }
 
     # This function has been created solely
@@ -565,20 +602,12 @@
         if (count($child_tasks) === 0) {
             echo '(NA)';
         } else {
-            $html = '';
-            foreach (
-                $child_tasks as $child_task => $_
-            ) {
-                $html
-                    .= '<li>'
-                    . get_task_html(
-                        $base_url,
-                        $tasks,
-                        $child_task
-                    )
-                    .'</li>';
-            }
-            echo "<ul>{$html}</ul>";
+            echo
+                get_task_list_html(
+                    $base_url,
+                    $child_tasks,
+                    $tasks
+                );
         }
     ?>
     <h2>ACTIONS</h2>
@@ -682,38 +711,30 @@
                     '(na)',
                     strtolower($_GET['target-task'])
                 ) !== false
-            ) $search_result[] = '(NA)';
+            ) $search_result['(NA)'] = null;
             $search_result
                 = array_merge(
                     $search_result,
-                    array_keys(
-                        array_filter(
-                            $tasks,
-                            fn ($task) =>
-                                strpos(
-                                    strtolower($task),
-                                    strtolower(
-                                        $_GET['target-task']
-                                    )
-                                ) !== false,
-                            ARRAY_FILTER_USE_KEY
-                        )
+                    array_filter(
+                        $tasks,
+                        fn ($task) =>
+                            strpos(
+                                strtolower($task),
+                                strtolower(
+                                    $_GET['target-task']
+                                )
+                            ) !== false,
+                        ARRAY_FILTER_USE_KEY
                     )
                 );
         }
-        $html = '';
         if (count($search_result) > 0) {
-            foreach ($search_result as $task) {
-                $html
-                    .= '<li>'
-                    . get_task_html(
-                        $base_url,
-                        $tasks,
-                        $task
-                    )
-                    .'</li>';
-            }
-            echo "<ul>{$html}</ul>";
+            echo
+                get_task_list_html(
+                    $base_url,
+                    $search_result,
+                    $tasks
+                );
         } else {
             echo '(NA)';
         }
@@ -936,38 +957,27 @@
     elseif ($_GET['view'] === 'all-task-view'):
 ?>
     <h1>ALL TASKS</h1>
-    <ul>
-        <?php
-            $url = $base_url;
-            $html
-                = '<li><a href="'
-                    . htmlspecialchars_with_ent_quotes(
-                        $url
-                    )
-                    . '">(NA)</a></li>';
-            foreach (
-                $tasks as $task => $details
-            ) {
-                $task_html
-                    = get_task_html(
-                        $base_url,
-                        $tasks,
-                        $task
-                    );
+    <?php
+        echo
+            get_task_list_html(
+                $base_url,
+                array_merge(
+                    [
+                        '(NA)' => null
+                    ],
+                    $tasks
+                ),
+                $tasks,
                 # Check if there is no task
                 #   with the parent task name.
-                if (
-                    $details[0] !== '(NA)'
-                        && !isset(
-                            $tasks[$details[0]]
-                        )
-                ) {
-                    $task_html
-                        = "(No task with the parent task name) {$task_html}";
-                }
-                $html .= "<li>{$task_html}</li>";
-            }
-            echo $html;
-        ?>
-    </ul>
+                fn ($task_html)
+                    =>
+                        $details[0] !== '(NA)'
+                            && !isset(
+                                $tasks[$details[0]]
+                            )
+                                ? "(No task with the parent task name) {$task_html}"
+                                : $task_html
+            );
+    ?>
 <?php endif; ?>

commit 6fcc38ab03ccdbf8bce0942d5a3cd03510d4601e

"get_task_list_item_content_html" change, bug fix
- The "get_task_list_item_content_html" function has been changed to expect that its callback return only the HTML of the decoration of a task list item content (before it returned the whole decorated task list item content HTML, i.e. the decoration together with the content). This way, I believe, the function is more intuitive.

- There has been fixed a bug of using the "details" variable in the arrow function given as an argument to the last call of the "get_task_list_html" function. This variable shouldn't be used there. The bug was introduced in commit bb23a695a21274c1fb97b9d2d86e92b2702eac0f, together with the introduction of the last call of the "get_task_list_html" function.

Worth to note, this bug was somehow hidden by the fact that the arrow function was capturing the "details" variable from the parent scope. Compare:

- https://www.php.net/manual/en/functions.arrow.php:

> when a variable used in the expression is defined in the parent scope it will be implicitly captured by-value

- https://www.php.net/manual/en/language.variables.scope.php:

> any variable defined outside a function is limited to the global scope.

So, to prevent such hiding, arrow functions should not be used. Instead, there can be used anonymous functions.

index.php
@@ -53,7 +53,7 @@
         $all_tasks_list,
         $task_name,
         ?callable
-            $get_decorated_task_list_item_content_html
+            $get_task_list_item_content_decoration_html
     ) {
         $task_name_html
             = htmlspecialchars_with_ent_quotes(
@@ -122,19 +122,19 @@
                 ];
         }
 
-        if (
-            $get_decorated_task_list_item_content_html
+        $decoration_html
+            = $get_task_list_item_content_decoration_html
                 !== null
-        ) {
-            $task_name_html
-                = $get_decorated_task_list_item_content_html(
-                    $task_name_html
-                );
-        }
+                ? $get_task_list_item_content_decoration_html(
+                    $task_name
+                )
+                    . ' '
+                : ' ';
 
         return
             "({$task_status_html}) "
             . $child_task_pending_information_html
+            . $decoration_html
             . "<a href=\"{$task_view_url_html}\">"
             . $task_name_html
             . '</a>';
@@ -145,7 +145,7 @@
         $task_list,
         $all_tasks_list,
         callable
-            $get_decorated_task_list_item_content_html
+            $get_task_list_item_content_decoration_html
                 = null
     ) {
         $task_list_content_html = '';
@@ -155,7 +155,7 @@
                     $base_url,
                     $all_tasks_list,
                     $task_name,
-                    $get_decorated_task_list_item_content_html
+                    $get_task_list_item_content_decoration_html
                 );
             $task_list_content_html
                 .= "<li>{$task_list_item_content_html}</li>";
@@ -1155,16 +1155,27 @@
                     $all_tasks_list
                 ),
                 $all_tasks_list,
-                # Check if there is no task
-                #   with the parent task name.
-                fn ($task_html)
-                    =>
-                        $details[0] !== '(NA)'
-                            && !isset(
-                                $all_tasks_list[$details[0]]
-                            )
-                                ? "(No task with the parent task name) {$task_html}"
-                                : $task_html
+                function ($task_name)
+                    use ($all_tasks_list) {
+                    if ($task_name !== '(NA)') {
+                        $parent_task_name
+                            = $all_tasks_list[$task_name][0];
+                        return
+                            # Check if there is no
+                            #   task
+                            #   with the parent
+                            #   task name.
+                            $parent_task_name
+                                !== '(NA)'
+                                && !isset(
+                                    $all_tasks_list[$parent_task_name]
+                                )
+                                    ? '(NO TASK WITH THE PARENT TASK NAME)'
+                                    : '';
+                    } else {
+                        return '';
+                    }
+                }
             );
     ?>
 <?php endif; ?>